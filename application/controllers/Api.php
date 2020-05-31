d49bd89fc4c04be935a0ea0f1933736bf1f51701
richard <tygalive@gmail.com>
2020-05-31 16:06:50 +0200

    change from analytics library to api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,5 +1,4 @@
 <?php
-use TheIconic\Tracking\GoogleAnalytics\Analytics;
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
@@ -9,8 +8,10 @@ class Api extends CI_Controller
     public function index()
     {
 
-        $this->load->library("PhpGoogleAnalytics");
+        $this->load->helper('url');
         $this->load->model("Rate_model", "rate");
+
+        $this->__logVisit();
 
         $source = $this->__normaliseName();
         $currency = $this->__normaliseCurrency();
@@ -19,43 +20,53 @@ class Api extends CI_Controller
 
         $sites = $this->rate->getByFilter($source, $currency, $date, $prefer);
 
+        header('Content-type: application/json');
+
         echo json_encode($sites->result());
 
-        $this->__logVisit();
-    }
-
-    private function __getIpAddress()
-    {
-        return filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP);
     }
 
     private function __logVisit()
     {
 
-        // Instantiate the Analytics object
-        // optionally pass TRUE in the constructor if you want to connect using HTTPS
-        $analytics = new Analytics(true);
+        ob_start();
 
-        // Build the GA hit using the Analytics class methods
-        // they should Autocomplete if you use a PHP IDE
-        $analytics
-            ->setProtocolVersion('1')
-            ->setTrackingId('UA-67829308-8')
-            ->setClientId('12')
-            ->setDocumentPath('/api')
-            ->setIpOverride($this->__getIpAddress());
+        $data = array(
+            "v" => 1, // Version.
+             "tid" => "UA-67829308-8", // Tracking ID / Property ID.
+             "dh" => base_url(), // Document hostname.
+             "cid" => $this->input->ip_address(), // Anonymous Client ID.
+             "t" => "pageview", // Hit Type.
+             "dp" => "api", // Page.
+        );
 
-        // When you finish bulding the payload send a hit (such as an pageview or event)
-        $analytics
-            ->setAsyncRequest(true)
-            ->sendPageview();
+        $headers[] = $this->input->user_agent() ?: "User-Agent:Zimrate/1.0";
+
+        // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, "https://www.google-analytics.com/collect");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // $output contains the output string
+        curl_exec($ch);
+
+        // close curl resource to free up system resources
+        curl_close($ch);
+
+        ob_clean();
     }
 
     private function __normaliseName()
     {
 
-        $source = $this->input->get("source");
-        $name = $this->input->get("name");
+        $source = $this->input->get_post("source");
+        $name = $this->input->get_post("name");
 
         //if source fails try name
         return strip_tags($source ?: ($name ?: ""));
@@ -66,7 +77,7 @@ class Api extends CI_Controller
      */
     private function __normaliseCurrency()
     {
-        $currency = strtoupper($this->input->get("currency"));
+        $currency = strtoupper($this->input->get_post("currency"));
 
         $currencies = $this->rate->getCurrencies();
 
@@ -82,7 +93,7 @@ class Api extends CI_Controller
     private function __normaliseDate()
     {
 
-        $date = $this->input->get("date");
+        $date = $this->input->get_post("date");
         if (!is_numeric($date)) {
             $date = "";
         }
@@ -98,7 +109,7 @@ class Api extends CI_Controller
     private function __normalisePrefer()
     {
 
-        $prefer = strtolower($this->input->get("prefer"));
+        $prefer = strtolower($this->input->get_post("prefer"));
 
         //value to get
         switch ($prefer) {
