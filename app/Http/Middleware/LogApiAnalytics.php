4835a551367f8223c754b7cba0a938a58cf515ca
Richard Muvirimi <richard@tyganeutronics.com>
2023-10-06 22:18:55 +0200

    Switch Google Analytics logger

--- app/Http/Middleware/LogApiAnalytics.php
+++ app/Http/Middleware/LogApiAnalytics.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use Br33f\Ga4\MeasurementProtocol\Dto\Event\BaseEvent;
+use Br33f\Ga4\MeasurementProtocol\Dto\Parameter\BaseParameter;
+use Br33f\Ga4\MeasurementProtocol\Dto\Request\BaseRequest;
+use Br33f\Ga4\MeasurementProtocol\Service;
 use Closure;
 use Exception;
-use Freshbitsweb\LaravelGoogleAnalytics4MeasurementProtocol\Facades\GA4;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class LogApiAnalytics
@@ -13,17 +17,67 @@ class LogApiAnalytics
     /**
      * Handle an incoming request.
      *
-     * @param  Closure(Request): (Response)  $next
+     * @param Closure(Request): (Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        try {
-            GA4::setClientId(request()->ip())
-                ->postEvent([
-                    'name' => 'api_request',
+
+        $measurementId = env('MEASUREMENT_ID', '');
+        $measurementSecret = env('MEASUREMENT_PROTOCOL_API_SECRET', '');
+
+        if (Str::of($measurementId)->isNotEmpty() && Str::of($measurementSecret)->isNotEmpty()) {
+            try {
+                ob_start();
+
+                $ip = $request->ip();
+                $userAgent = $request->userAgent() ?: 'Zimrate/1.0';
+
+                $ga4Service = new Service($measurementSecret);
+                $ga4Service->setMeasurementId($measurementId);
+                $ga4Service->setIpOverride($ip);
+                $ga4Service->setOptions([
+                    'User-Agent' => $userAgent,
                 ]);
-        } catch (Exception) {
-            // Do nothing
+
+                $baseRequest = new BaseRequest();
+                $baseRequest->setClientId($ip);
+
+                $sessionId = md5($ip . $userAgent);
+
+                $baseRequest->setUserId($sessionId);
+
+                $baseEvent = new BaseEvent('api_request');
+
+                // Create Base Event
+                $paramSessionId = new BaseParameter($sessionId);
+                $baseEvent->addParam('engagement_time_msec', $paramSessionId);
+
+                $paramEngagementTime = new BaseParameter('100');
+                $baseEvent->addParam('session_id', $paramEngagementTime);
+
+                $baseRequest->addEvent($baseEvent);
+
+                // Create View Page Event
+                $pageViewEvent = new BaseEvent('page_view');
+
+                $pageViewParam = new BaseParameter($request->url());
+                $pageViewEvent->addParam('page_location', $pageViewParam);
+
+                $localeParam = new BaseParameter($request->getLocale());
+                $pageViewEvent->addParam('language', $localeParam);
+
+                $titleParam = new BaseParameter($request->path());
+                $pageViewEvent->addParam('page_title', $titleParam);
+
+                $baseRequest->addEvent($pageViewEvent);
+
+                // Send
+                $ga4Service->send($baseRequest);
+            } catch (Exception) {
+                //do nothing
+            } finally {
+                ob_clean();
+            }
         }
 
         return $next($request);
--- database/migrations/2014_09_12_181218_create_zimrate_table.php
+++ database/migrations/2014_09_12_181218_create_zimrate_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        if (!Schema::hasTable('zimrate')) {
+        if (! Schema::hasTable('zimrate')) {
             Schema::create('zimrate', function (Blueprint $table) {
                 $table->id();
                 $table->boolean('status')->default(false);
