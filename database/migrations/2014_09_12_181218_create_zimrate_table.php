f1d21644530af0808d3911f43e07b94f2d31d921
Richard Muvirimi <richard@tyganeutronics.com>
2023-10-06 21:48:44 +0200

    Fix: set rate and last_rate columns to have null constraints

--- database/migrations/2014_09_12_181218_create_zimrate_table.php
+++ database/migrations/2014_09_12_181218_create_zimrate_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        if (! Schema::hasTable('zimrate')) {
+        if (!Schema::hasTable('zimrate')) {
             Schema::create('zimrate', function (Blueprint $table) {
                 $table->id();
                 $table->boolean('status')->default(false);
@@ -20,7 +19,7 @@ return new class extends Migration
                 $table->string('currency');
                 $table->string('url');
                 $table->string('selector');
-                $table->float('rate');
+                $table->float('rate', null, null);
                 $table->integer('last_checked');
                 $table->string('last_updated_selector');
                 $table->integer('last_updated');
--- database/migrations/2023_10_03_175714_create_last_rate_column.php
+++ database/migrations/2023_10_03_175714_create_last_rate_column.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('rates', function (Blueprint $table) {
-            $table->float('last_rate')->after('rate')->default(1);
+            $table->float('last_rate', null, null)->after('rate')->default(1);
         });
     }
 
