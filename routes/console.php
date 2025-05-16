90ec5e8afa875b496e8faef3a75df3d10a3cf31d
Richard Muvirimi <richard@tyganeutronics.com>
2025-05-16 10:26:36 +0200

    Release 1.0.5 Add scheduled commands for pruning, scraping, and status updates in console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('sanctum:prune-expired --hours=24')->hourly();
+Schedule::command('app:scrape')->hourly()->between('8:00', '20:00')->timezone('Africa/Harare');
+
+Schedule::command('app:status')->daily()->at('20:00');
