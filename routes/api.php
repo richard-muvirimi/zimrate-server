c4c902afa5416034063427031a88b27e32928e8d
richard-muvirimi <19529979+richard-muvirimi@users.noreply.github.com>
2025-05-25 15:02:20 +0000

    fix code style

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\OptionController;
 use App\Http\Controllers\RateController;
 use App\Http\Controllers\RatesController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('setup', [Controller::class, 'setup']);
 
