63fa1e8adb9cd3bc508ce92b2acb046a84ac8418
Richard Muvirimi <richard@tyganeutronics.com>
2025-05-25 17:01:19 +0200

    feat: integrate Laravel Sanctum for API authentication and add personal access tokens migration

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\OptionController;
 use App\Http\Controllers\RateController;
 use App\Http\Controllers\RatesController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 Route::get('setup', [Controller::class, 'setup']);
 
 Route::post('/login', [AuthController::class, 'login']);
