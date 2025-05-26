013d1b0c470d6ded80f776f707b66abed3e267c6
richard-muvirimi <19529979+richard-muvirimi@users.noreply.github.com>
2025-05-26 11:48:28 +0000

    fix code style

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Middleware\LogApiAnalytics;
-use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
